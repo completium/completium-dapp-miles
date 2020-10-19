977cc3d871fcc4ae8e06feabc52dd9caaa071e2a
Benoît Rognier <benoit.rognier@edulera.com>
2020-10-19 15:48:42 +0200

    adds settings

---
+++ miles/src/settings.js
@@ -0,0 +1,34 @@
+export const  contractAddress = "KT1 this is an address"
+
+export const  accountAddress  = "tz1 this is an address"
+
+export const  defaultNbActiveMiles = 5
+
+export const  milesColumns    = [
+    { field: 'id', headerName: 'ID', width: 100 },
+    { field: 'quantity', headerName: 'Quantity', width: 200 },
+    { field: 'expiration', headerName: 'Expiration', width: 200 },
+  ]
+
+export const defaultMiles =  [
+    { id : "M0000001", expiration : "2020-06-01", quantity : 3 },
+    { id : "M0000002", expiration : "2020-10-21", quantity : 4 },
+    { id : "M0000003", expiration : "2020-12-02", quantity : 2 },
+    { id : "M0000003", expiration : "2020-12-03", quantity : 2 },
+    { id : "M0000003", expiration : "2020-12-04", quantity : 2 },
+    { id : "M0000003", expiration : "2020-12-05", quantity : 2 },
+    { id : "M0000003", expiration : "2020-12-06", quantity : 2 },
+    { id : "M0000003", expiration : "2020-12-07", quantity : 2 },
+  ]
+
+export const products = [
+    { pid: "P001", image : "tezos-case.jpg",  title : "Tezos Phone Case", nbmiles : 5, got : false },
+    { pid: "P002", image : "archetype-mug.jpg",  title : "Archetype Mug", nbmiles : 3, got : false },
+    { pid: "P003", image : "tezos-sweats.jpg", title : "Tezos Sweat",      nbmiles : 8, got : false },
+    { pid: "P004", image : "archetype-cap.png",  title : "Archetype Cap", nbmiles : 2, got : false },
+    { pid: "P005", image : "tezos-cap.png",  title : "Tezos Cap", nbmiles : 2, got : false },
+  ]
+
+export const githubUrl = "https://github.com/edukera/completium-dapps"
+
+export const appTitle = "Use Your Miles Before Expiration!"
