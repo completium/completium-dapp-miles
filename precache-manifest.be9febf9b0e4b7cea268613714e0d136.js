dfba26fbaf6f07d6ed172a31b7502b51d6e064fe
Benoît Rognier <benoit.rognier@edulera.com>
2020-12-06 09:13:23 +0100

    Updates

---
+++ precache-manifest.be9febf9b0e4b7cea268613714e0d136.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d3d9c1e31549a074b3547d066a0020",
+    "url": "/completium-dapp-miles/index.html"
+  },
+  {
+    "revision": "69bee445b5b00fdf6a30",
+    "url": "/completium-dapp-miles/static/css/main.4aae50fe.chunk.css"
+  },
+  {
+    "revision": "aae1ed356f3acd552843",
+    "url": "/completium-dapp-miles/static/js/2.d800cfcc.chunk.js"
+  },
+  {
+    "revision": "fbd8c098fb9181724451f41953b32d5c",
+    "url": "/completium-dapp-miles/static/js/2.d800cfcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69bee445b5b00fdf6a30",
+    "url": "/completium-dapp-miles/static/js/main.e97f01f5.chunk.js"
+  },
+  {
+    "revision": "48283ae1b6f690c45209",
+    "url": "/completium-dapp-miles/static/js/runtime-main.dde7a563.js"
+  },
+  {
+    "revision": "7b9fd4ece3c8b85684a709c894bdf36d",
+    "url": "/completium-dapp-miles/static/media/takeoff-ticket.7b9fd4ec.svg"
+  }
+]);
